8feb25a737b16387d9ee77523867e2abc31dd664
Frantz36 <dimitri.tapamo@gmail.com>
2024-01-04 17:58:51 +0100

    debugged frontend

--- FrontEnd/src/pages/Sign-up.js
+++ FrontEnd/src/pages/Sign-up.js
@@ -1,27 +1,29 @@
-    import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect } from 'react';
 import "../styles/pages/sign-up.css"
-import ReactDOM from "react-dom";
+import "https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"
 
 function SignUp() {
 
-    const containers = document.getElementById('container');
-    const overlayBtns = document.getElementById('overlayBtn');
-    const overlayCons = document.getElementById('overlayCons');
-
-    const container = ReactDOM.findDOMNode(containers);
-    const overlayCon = ReactDOM.findDOMNode(overlayCons);
-    const overlayBtn = ReactDOM.findDOMNode(overlayBtns);
-
+    var container
+    useEffect(() => {
+        container = document.getElementById("container")
+        console.log(container)
+    }, [])
+    
+    
     return (
+        
         <div>
             <div className="container" id="container">
                 <div className="form-container sign-up-container">
                     <form action="#">
                         <h1>Create Account</h1>
                         <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="#" className="social"><box-icon type='logo' name='facebook' className="icon"></box-icon></a>
+                            <a href="#" className="social"><box-icon type='logo' name='google-plus'></box-icon></a>
+                            <a href="#" className="social"><box-icon name='linkedin' type='logo' ></box-icon></a>
                         </div>
                         <span>or use your email for registration</span>
                         <div className="infield">
@@ -43,9 +45,9 @@ function SignUp() {
                     <form action="#">
                         <h1>Sign in</h1>
                         <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="#" className="social"><box-icon type='logo' name='facebook' className="icon"></box-icon></a>
+                            <a href="#" className="social"><box-icon type='logo' name='google-plus'></box-icon></a>
+                            <a href="#" className="social"><box-icon name='linkedin' type='logo' ></box-icon></a>
                         </div>
                         <span>or use your account</span>
                         <div className="infield">
@@ -73,13 +75,12 @@ function SignUp() {
                             <button>Sign Up</button>
                         </div>
                     </div>
-                    <button id="overlayBtn" onClick={ () => {
-            container.classList.toggle('right-panel-active');
-        }} ></button>
+                    <button id="overlayBtn" onClick={ () => {container.classList.toggle("right-panel-active")}} ></button>
                 </div>
             </div>
         </div>
     );
+
 };
 
 export default SignUp;
