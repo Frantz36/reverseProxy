8b1ff7d102f18be44a6b4ff1dd3584eed4e8e2a6
NyemeckJames <jamesnyemeck@gmail.com>
2024-01-27 16:38:48 +0100

    Gestion des guards

--- BackEnd/reverse.js
+++ BackEnd/reverse.js
@@ -1,1 +1,20 @@
-console.log("Push")
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+
+const IsAuthGuard = ({children})=>{
+    console.log("Bonjour")
+    const navigate = useNavigate();
+    
+    const [userData, setUserData] = useState(null)
+    useEffect(()=>{
+        if (localStorage.getItem('userData')) {
+            setUserData(JSON.parse(localStorage.getItem('userData')));
+            navigate("/dashboard")
+        }
+    },[])
+
+    return children
+}
+
+export default IsAuthGuard;
